37b9f5e6ebc9ad8027df69ce9351d44f3b7efdde
Luke Kuzmish <lakuzmish@gmail.com>
2025-03-17 20:48:05 -0400

    QueueFake::listenersPushed()

--- src/Illuminate/Support/Testing/Fakes/QueueFake.php
+++ src/Illuminate/Support/Testing/Fakes/QueueFake.php
@@ -5,6 +5,7 @@ namespace Illuminate\Support\Testing\Fakes;
 use BadMethodCallException;
 use Closure;
 use Illuminate\Contracts\Queue\Queue;
+use Illuminate\Events\CallQueuedListener;
 use Illuminate\Queue\CallQueuedClosure;
 use Illuminate\Queue\QueueManager;
 use Illuminate\Support\Collection;
@@ -238,6 +239,19 @@ class QueueFake extends QueueManager implements Fake, Queue
     }
 
     /**
+     * @param  class-string  $listener
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public function assertListenerPushed($listener, $callback = null)
+    {
+        $this->assertPushed(
+            CallQueuedListener::class,
+            fn (CallQueuedListener $callQueuedListener) => $callQueuedListener->class === $listener && $callback($callQueuedListener)
+        );
+    }
+
+    /**
      * Assert that a closure was not pushed based on a truth-test callback.
      *
      * @param  callable|null  $callback
@@ -324,6 +338,29 @@ class QueueFake extends QueueManager implements Fake, Queue
         return (new Collection($this->jobs[$job]))->filter(
             fn ($data) => $callback($data['job'], $data['queue'], $data['data'])
         )->pluck('job');
+    }
+
+    /**
+     * Get all of the jobs by listener class, passing an optional truth-test callback.
+     *
+     * @param  class-string  $listenerClass
+     * @param  (\Closure(CallQueuedListener, string|null, mixed): bool)|null  $callback
+     * @return \Illuminate\Support\Collection<int, object>
+     */
+    public function listenersPushed($listenerClass, $callback = null)
+    {
+        if (! $this->hasPushed(CallQueuedListener::class)) {
+            return new Collection;
+        }
+
+        $collection = (new Collection($this->jobs[CallQueuedListener::class]))
+            ->filter(fn ($data) => $data['job']->class === $listenerClass);
+
+        if ($callback) {
+            $collection = $collection->filter(fn ($data) => $callback($data['job'], $data['queue'], $data['data']));
+        }
+
+        return $collection->pluck('job');
     }
 
     /**
